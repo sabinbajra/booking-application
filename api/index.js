bffdb199c65ba6b1d6b63099f6601a5bc9affb6e
sabin bajracharya <sabin1129@hotmail.com>
2023-04-19 00:09:38 +0200

    user registration successfull

--- api/index.js
+++ api/index.js
@@ -1,7 +1,39 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const UserModel = require('./models/User');
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const bcryptSalt = bcrypt.genSaltSync(10);
 
-app.get('/tests',(req,res) => {
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000',
+}));
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.post('/register',async (req,res) => {
+    const { name, email, password } = req.body;
+    try{
+
+        const userDoc = await UserModel.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password,bcryptSalt),
+        });
+        res.json({userDoc});
+    }
+    catch(error){
+        res.status(422).json(error);
+    }
+    
+   
+});
+
+app.get('/test',(req,res) => {
     res.json('test ok');
 });
 
--- booking-app/src/pages/RegisterPage.js
+++ booking-app/src/pages/RegisterPage.js
@@ -1,17 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const RegisterPage = () => {
+
+    const [name,setName] = useState("");
+    const [password,setPassword] = useState("");
+    const [email,setEmail] = useState("");
+
+    const registeruser = async (event) => {
+        console.log("inside register user");
+        event.preventDefault();
+        await axios.post('/register', {
+            name,
+            email,
+            password,
+        });
+        alert("Registration successfull. Now you can login!!!");
+    };
+
   return (
     <div className='mt-4 grow flex items-center justify-around'>
         <div className='mb-64'>
 
             <h1 className='text-4xl text-center mb-4'>Register</h1>
-            <form className='max-w-md mx-auto '>
-                <input type='email' placeholder='Your Name Please ...'></input>
-                <input type='email' placeholder='Your Email Please ...'></input>
-                <input type='password' placeholder='Your Password Please ...'></input>
-                <button className='primary'>Login</button>
+            <form className='max-w-md mx-auto ' onSubmit={registeruser}>
+                <input 
+                type='text' 
+                placeholder='Your Name Please  ...' 
+                value={name} 
+                onChange={event => setName(event.target.value)}></input>
+                
+                <input 
+                type='email' 
+                placeholder='Your Email Please ...' 
+                value={email} 
+                onChange={event => setEmail(event.target.value)}></input>
+                
+                <input 
+                type='password' 
+                placeholder='Your Password Please ...'
+                value={password} 
+                onChange={event => setPassword(event.target.value)}></input>
+                
+                <button className='primary'>Register</button>
+
                 <div className='text-center py-2 text-gray-500'>
                     Already a member? <Link className="underline text-black" to={'/login'}>Login</Link>
                 </div>
