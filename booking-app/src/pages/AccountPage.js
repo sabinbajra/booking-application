8f09e1a3a685c9013aed33e3d4c52bac5b22de3f
sabin bajracharya <sabin1129@hotmail.com>
2023-04-20 19:42:50 +0200

    css for myprofile booking and places 2:00:00

--- booking-app/src/pages/AccountPage.js
+++ booking-app/src/pages/AccountPage.js
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../UserContext'
-import { Navigate } from 'react-router-dom';
+import { Link, Navigate, useParams } from 'react-router-dom';
 
 const AccountPage = () => {
 
     const {ready, user} = useContext(UserContext);
+    const {subpage} = useParams();
 
     if(!ready)
     {
@@ -13,8 +14,25 @@ const AccountPage = () => {
     if(ready && !user){
         return <Navigate to={'/login'}/>
     }
+
+    const linikClasses = (type=null) => {
+        let classes = 'py-2 px-6';
+        if(type === subpage || (subpage === undefined && type === 'profile'))
+        {
+            classes += ' bg-primary text-white rounded-full';
+        }
+        return classes;
+    }
+    
   return (
-    <div>AccountPage for {user?.name} </div>
+    <div> 
+        <nav className='w-full flex justify-center mt-8 gap-4'>
+            <Link className={linikClasses('profile')} to={'/account'}>My Profile</Link>
+            <Link className={linikClasses('bookings')} to={'/account/bookings'}>My Booking</Link>
+            <Link className={linikClasses('places')} to={'/account/places'}>My Accomodations</Link>
+          
+        </nav>
+    </div>
   )
 }
 
